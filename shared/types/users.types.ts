9abdaf8ef76c77eb3957d2ad643a859d9fd98a86
Katleho Myeza <katleho@synthesis.co.za>
2025-10-29 15:20:54 +0200

    added googleId to users table and property

--- shared/types/users.types.ts
+++ shared/types/users.types.ts
@@ -4,6 +4,7 @@ export type User = {
   id : UUID
   name : string
   email : string
+  googleId: string
 }
 
-export type NewUser = Pick<User,"name"|"email">
+export type NewUser = Pick<User,"name"|"email"|"googleId">
