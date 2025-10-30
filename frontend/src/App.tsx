ed373ad4676725c800486e15b95d3db8078836dd
Katleho Myeza <katleho@synthesis.co.za>
2025-10-30 16:27:52 +0200

    added side navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,34 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import "./App.css";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Dashboard from "./pages/Dashboard/DashBoard";
+import Tracker from "./pages/Tracker/Tracker";
+import Goals from "./pages/Goals/Goals";
+import { Layout } from "./Layout";
+import Projects from "./pages/Projects/Projects";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/landing" element={<LandingPage />} />
+        
         <Route
-          path="/home"
+          path="/*"
           element={
             <ProtectedRoute>
-              <Dashboard />
+              <Layout>
+                <Routes>
+                  <Route path="/home" element={<Dashboard />} />
+                  <Route path="/tracker" element={<Tracker />} />
+                  <Route path="/projects" element={<Projects />} />
+                  <Route path="/goals" element={<Goals />} />
+                  <Route path="*" element={<Navigate to="/home" replace />} />
+                </Routes>
+              </Layout>
             </ProtectedRoute>
           }
         />
-        <Route path="*" element={<Navigate to="/home" replace />} />
       </Routes>
     </BrowserRouter>
   );
