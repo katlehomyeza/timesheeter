988f336fc51500ea8fcb955ace0f882b7d39900a
Katleho Myeza <katleho@synthesis.co.za>
2025-11-02 19:59:57 +0200

    added project backend functionality

--- backend/queries/project.queries.ts
+++ backend/queries/project.queries.ts
@@ -5,7 +5,7 @@ import { pool } from "../config/pool";
 export async function createNewProject(
   userId: User["id"],
   projectName: Project['name'],
-  color_hex: Project['colorHex'],
+  colorHex: Project['colorHex'],
   description?: Project['description']
 ): Promise<Project> {
   const query = `
@@ -21,8 +21,60 @@ export async function createNewProject(
         created_at as "createdAt";
   `;
 
-  const values = [userId, projectName, description ?? null, color_hex];
+  const values = [userId, projectName, description ?? null, colorHex];
   const result = await pool.query(query, values);
   return result.rows[0];
 }
 
+export async function deleteProject(
+  projectId: Project["id"]
+): Promise<Project> {
+  const query = `
+    DELETE FROM projects
+    WHERE project_id = $1
+    RETURNING 
+      project_id AS "id", 
+      user_id AS "userId", 
+      name, 
+      description, 
+      color_hex AS "colorHex", 
+      created_at AS "createdAt";
+  `;
+
+  const result = await pool.query(query, [projectId]);
+  return result.rows[0] ?? null;
+}
+
+export async function isUsersProject(
+  userId: User["id"],
+  projectId: Project["id"]
+): Promise<boolean> {
+  const query = `
+    SELECT 1
+    FROM projects
+    WHERE user_id = $1 AND project_id = $2
+    LIMIT 1;
+  `;
+
+  const result = await pool.query(query, [userId, projectId]);
+  return (result.rowCount !== null && result.rowCount > 0);
+}
+export async function getAllProjects(
+  userId: User["id"],
+): Promise<Project[]> {
+  const query = `
+    SELECT 
+      project_id AS "id",
+      user_id AS "userId",
+      name,
+      description,
+      color_hex AS "colorHex",
+      created_at AS "createdAt"
+    FROM projects
+    WHERE user_id = $1
+    ORDER BY created_at DESC;
+  `;
+
+  const result = await pool.query(query, [userId]);
+  return result.rows;
+}
