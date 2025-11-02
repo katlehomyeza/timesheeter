e692ecfe337ed47f82eb70a104bf4a8555b4ba23
Katleho Myeza <katleho@synthesis.co.za>
2025-11-02 19:59:57 +0200

    added project backend functionality

--- backend/index.ts
+++ backend/index.ts
@@ -2,11 +2,14 @@ import express from "express";
 import cors from "cors"
 import authRoutes from "./routes/auth.routes";
 import userRoutes from "./routes/auth.routes";
+import projectRoutes from "./routes/project.routes";
 
 const app = express();
 app.use(cors())
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/projects", projectRoutes);
 
 app.listen(4000, () => console.log("Server running on 4000"));
---
+++ backend/routes/project.routes.ts
@@ -0,0 +1,50 @@
+import express from "express"
+import { createNewProject, deleteProject, getAllProjects, isUsersProject } from "../queries/project.queries";
+import { authenticateToken } from "../middleware/auth";
+
+const router = express.Router();
+router.use(authenticateToken)
+
+router.post("/", async (req, res) => {
+    try {
+        const { projectName, colorHex, description } = req.body;
+        const userId = req.authUser!.userId;
+        const createdProject = await createNewProject(userId, projectName, colorHex, description);
+        res.status(201).json(createdProject);
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong", detail:error });
+    }
+});
+
+router.get("/", async (req, res) => {
+    try {
+        const userId = req.authUser!.userId;
+        const projects = await getAllProjects(userId);
+        res.status(200).json(projects);
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" });
+    }
+});
+
+
+router.delete("/:projectId", async (req,res) => {
+    try {
+        const {projectId} = req.params;
+        const {userId} = req.authUser!;
+        if (!projectId){
+            res.status(400).json({ message: "Please enter the project ID" });
+        } else {
+            const isUserProject = await isUsersProject(userId, projectId);
+            if(isUserProject){
+                const deletedProject = await deleteProject(projectId);
+                res.status(200).json(deletedProject);
+            } else {
+                res.status(401).json({message:"You can only delete your own projects"});
+            }
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" });
+    }
+});
+
+export default router;
