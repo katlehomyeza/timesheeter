03114d73593780a677afbd482114b382d8f56881
Katleho Myeza <katleho@synthesis.co.za>
2025-11-04 11:44:20 +0200

    added tracker frontend service

---
+++ frontend/src/services/timelog.service.ts
@@ -0,0 +1,62 @@
+import type { TimeLog } from "@shared/types/project.types";
+import { isErrorDetail, type ErrorDetail } from "@shared/types/utility.types";
+import API from "../utils/api";
+
+export async function createTimeLog(
+  projectId: string,
+  options: {
+    durationMinutes?: number;
+    startTime?: Date;
+    endTime?: Date;
+    note?: string;
+    goalId?: string;
+    isManualEntry: boolean;
+  }
+): Promise<TimeLog | ErrorDetail> {
+  try {
+    return await API.post("/timelogs", {
+      projectId,
+      ...options,
+    });
+  } catch (error) {
+    if (isErrorDetail(error)) {
+      return error;
+    } else {
+      return {
+        message: "Failed to create time log: Please try again later",
+      };
+    }
+  }
+}
+
+export async function deleteTimelog(
+  timeLogId: string
+): Promise<TimeLog | ErrorDetail> {
+  try {
+    return await API.delete(`/timelogs/${timeLogId}`);
+  } catch (error) {
+    if (isErrorDetail(error)) {
+      return error;
+    } else {
+      return {
+        message: "Failed to delete time log: Please try again later",
+      };
+    }
+  }
+}
+
+export async function getTimeLogs(
+  projectId: string
+): Promise<TimeLog[] | ErrorDetail> {
+  try {
+    return await API.get(`/timelogs/${projectId}`);
+  } catch (error) {
+    if (isErrorDetail(error)) {
+      return error;
+    } else {
+      return {
+        message: "Failed to fetch time logs: Please try again later",
+      };
+    }
+  }
+}
